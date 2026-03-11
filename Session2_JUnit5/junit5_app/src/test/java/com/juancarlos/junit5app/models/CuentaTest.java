76f4c69072790bbf22e8d4238b89d6dc1267ac67
Juan Carlos <juancarlosfilter@gmail.com>
2026-03-11 18:50:38 +0100

    session2 class banco, class exception

--- Session2_JUnit5/junit5_app/src/test/java/com/juancarlos/junit5app/models/CuentaTest.java
+++ Session2_JUnit5/junit5_app/src/test/java/com/juancarlos/junit5app/models/CuentaTest.java
@@ -1,5 +1,6 @@
 package com.juancarlos.junit5app.models;
 
+import com.juancarlos.junit5app.exceptions.DineroInsuficienteException;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
@@ -14,9 +15,96 @@ class CuentaTest {
 //        cuenta.setPersona("JuanCarlos");
         String esperado = "JuanCarlos";
         String real = cuenta.getPersona();
+        assertNotNull(real);
         assertEquals(esperado, real);
         assertTrue(real.equals("JuanCarlos"));
 
     }
 
+    @Test
+    void testSaldoCuenta() {
+        Cuenta cuenta = new Cuenta("JuanCarlos", new BigDecimal("1000.12345"));
+        assertNotNull(cuenta.getSaldo());
+        assertEquals(1000.12345, cuenta.getSaldo().doubleValue());
+        assertFalse(cuenta.getSaldo().compareTo(BigDecimal.ZERO) < 0);
+        assertTrue(cuenta.getSaldo().compareTo(BigDecimal.ZERO) > 0);
+    }
+
+    @Test
+    void testReferenciaCuenta() {
+        Cuenta cuenta = new Cuenta("John Doe", new BigDecimal("8900.9997"));
+        Cuenta cuenta2 = new Cuenta("John Doe", new BigDecimal("8900.9997"));
+
+//        assertNotEquals(cuenta2, cuenta);
+        assertEquals(cuenta2, cuenta);
+
+    }
+
+    @Test
+    void testDebidoCuenta() {
+        Cuenta cuenta = new Cuenta("JuanCarlos", new BigDecimal("1000.12345"));
+        cuenta.debito(new BigDecimal(100));
+        assertNotNull(cuenta.getSaldo());
+        assertEquals(900, cuenta.getSaldo().intValue());
+        assertEquals("900.12345", cuenta.getSaldo().toPlainString());
+    }
+
+    @Test
+    void testCreditoCuenta() {
+        Cuenta cuenta = new Cuenta("JuanCarlos", new BigDecimal("1000.12345"));
+        cuenta.credito(new BigDecimal(100));
+        assertNotNull(cuenta.getSaldo());
+        assertEquals(1100, cuenta.getSaldo().intValue());
+        assertEquals("1100.12345", cuenta.getSaldo().toPlainString());
+    }
+
+    @Test
+    void dineroInsuficienteException() {
+        Cuenta cuenta = new Cuenta("JuanCarlos", new BigDecimal("1000.12345"));
+        Exception exception = assertThrows(DineroInsuficienteException.class, () -> {
+            cuenta.debito(new BigDecimal(1500));
+        });
+        String actual = exception.getMessage();
+        String esperado = "Dinero Insuficiente";
+        assertEquals(esperado, actual);
+    }
+
+    @Test
+    void testTransferirDineroCuenta() {
+        Cuenta cuenta1 = new Cuenta("John Doe", new BigDecimal("2500"));
+        Cuenta cuenta2 = new Cuenta("Andres", new BigDecimal("1500.9898"));
+
+        Banco banco = new Banco();
+        banco.setNombre("Banco del Estado");
+        banco.transferir(cuenta2, cuenta1, new BigDecimal(500));
+        assertEquals("1000.9898", cuenta2.getSaldo().toPlainString());
+        assertEquals("3000", cuenta1.getSaldo().toPlainString());
+    }
+
+    @Test
+    void testRelacionBancoCuenta() {
+        Cuenta cuenta1 = new Cuenta("John Doe", new BigDecimal("2500"));
+        Cuenta cuenta2 = new Cuenta("JuanCarlos", new BigDecimal("1500.9898"));
+
+        Banco banco = new Banco();
+        banco.addCuenta(cuenta1);
+        banco.addCuenta(cuenta2);
+
+
+        banco.setNombre("Banco del Estado");
+        banco.transferir(cuenta2, cuenta1, new BigDecimal(500));
+        assertEquals("1000.9898", cuenta2.getSaldo().toPlainString());
+        assertEquals("3000", cuenta1.getSaldo().toPlainString());
+
+        assertEquals(2, banco.getCuentas().size());
+        assertEquals("Banco del Estado", cuenta1.getBanco().getNombre());
+
+        assertEquals("JuanCarlos", banco.getCuentas().stream()
+                .filter(c -> c.getPersona().equals("JuanCarlos"))
+                .findFirst()
+                .get().getPersona());
+
+        assertTrue(banco.getCuentas().stream()
+                .anyMatch(c -> c.getPersona().equals("JuanCarlos")));
+    }
 }
